6241d140f23ce3fc6544df710c1f61573a39129f
elias <eliasnsarmo@gmail.com>
2017-06-28 09:48:14 +0000

    step7

---
+++ Base1/step_7.js
@@ -0,0 +1,8 @@
+function user(){
+
+    var number1=parseInt (document.getElementById("shoe_size").value);
+    var multi=(((number1*2)+5)*50);
+    var number2=parseInt (document.getElementById("year").value);
+    var adds1766=(number2+1766);
+    alert("( shoe size : "+multi+" )"+"(year  : "+adds1766+" )");
+}
