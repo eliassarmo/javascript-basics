e7b42c08c7e376c73ba8d38ab74b5c70522636df
elias <eliasnsarmo@gmail.com>
2017-06-28 09:36:31 +0000

    step6

---
+++ Base1/Step_6.js
@@ -0,0 +1,7 @@
+function result(){
+ 
+ var number1=parseInt(document.getElementById("first_number").value);
+ var number2=parseInt(document.getElementById("second_number").value);
+ var result=(number1/number2);
+ alert("Result = "+result);
+ } 
